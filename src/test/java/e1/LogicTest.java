8cc52b2689712e2aef53a737687dbb25d63c77aa
Angelo Tinti <angelo.tinti@studio.unibo.it>
2024-03-04 12:06:46 +0100

    Add test for knight movement

--- src/test/java/e1/LogicTest.java
+++ src/test/java/e1/LogicTest.java
@@ -4,29 +4,50 @@ import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.NoSuchElementException;
+import java.util.stream.IntStream;
 
 public class LogicTest {
 
   private Logics logic;
   private int fieldSize = 5;
-  private Pair<Integer, Integer> kingPosition;
+  private Pair<Integer, Integer> knightPosition;
 
   @BeforeEach
   public void initializeLogic() {
     this.logic = new LogicsImpl(fieldSize);
-    this.kingPosition = this.getKingPosition();
+    this.knightPosition = this.getKingPosition();
   }
 
   @Test
   public void testKingPosition() {
-    assertTrue(this.logic.hasKnight(this.kingPosition.getX(), this.kingPosition.getY()));
+    assertTrue(this.logic.hasKnight(this.knightPosition.getX(), this.knightPosition.getY()));
   }
 
   @Test
   public void testWrongKingPosition() {
-    Pair<Integer, Integer> wrongPosition = new Pair<Integer, Integer>((this.kingPosition.getX() + 1) % fieldSize,
-        (this.kingPosition.getY() + 1) % fieldSize);
+    Pair<Integer, Integer> wrongPosition = new Pair<Integer, Integer>((this.knightPosition.getX() + 1) % fieldSize,
+        (this.knightPosition.getY() + 1) % fieldSize);
     assertFalse(this.logic.hasKnight(wrongPosition.getX(), wrongPosition.getY()));
+  }
+
+  @Test
+  public void testMovement() {
+    int newRowKnight = this.knightPosition.getX() + 2;
+    int newColumnKnight = this.knightPosition.getY() + 1;
+    Pair<Integer, Integer> newKnightPosition = new Pair<Integer, Integer>(newRowKnight, newColumnKnight);
+    if (this.isPositionInsideField(newKnightPosition)) {
+      this.logic.hit(newRowKnight, newColumnKnight);
+      this.knightPosition = this.getKingPosition();
+      assertEquals(newKnightPosition, this.knightPosition);
+    } else {
+      assertThrows(IndexOutOfBoundsException.class, () -> this.logic.hit(newRowKnight, newColumnKnight));
+    }
+  }
+
+  @Test
+  public void testMovements() {
+    int movementsNumber = 20;
+    IntStream.range(0, movementsNumber).forEach(step -> this.testMovement());
   }
 
   private Pair<Integer, Integer> getKingPosition() {
@@ -42,5 +63,10 @@ public class LogicTest {
       throw new NoSuchElementException();
     }
     return position;
+  }
+
+  private boolean isPositionInsideField(Pair<Integer, Integer> position) {
+    return position.getX() >= 0 && position.getX() < fieldSize
+        && position.getY() >= 0 && position.getY() < fieldSize;
   }
 }
