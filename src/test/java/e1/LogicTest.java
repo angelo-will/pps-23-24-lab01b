faa123dfad1db22f27ed44e5718ac08ac5a73870
Angelo Tinti <angelo.tinti@studio.unibo.it>
2024-03-04 12:49:32 +0100

    Add in test class method to find pawn and relative test on it

--- src/test/java/e1/LogicTest.java
+++ src/test/java/e1/LogicTest.java
@@ -12,27 +12,41 @@ public class LogicTest {
   private Logics logic;
   private int fieldSize = 5;
   private Pair<Integer, Integer> knightPosition;
+  private Pair<Integer, Integer> pawnPositions;
 
   @BeforeEach
   public void initializeLogic() {
     this.logic = new LogicsImpl(fieldSize);
     this.knightPosition = this.getKnightPosition();
+    this.pawnPositions = this.getPawnPosition();
   }
 
   @Test
   public void testKingPosition() {
     assertTrue(this.logic.hasKnight(this.knightPosition.getX(), this.knightPosition.getY()));
   }
-
+  
   @Test
   public void testWrongKingPosition() {
     Pair<Integer, Integer> wrongPosition = new Pair<Integer, Integer>((this.knightPosition.getX() + 1) % fieldSize,
-        (this.knightPosition.getY() + 1) % fieldSize);
+    (this.knightPosition.getY() + 1) % fieldSize);
     assertFalse(this.logic.hasKnight(wrongPosition.getX(), wrongPosition.getY()));
+  }
+  
+  @Test
+  public void testPawnPosition() {
+    assertTrue(this.logic.hasPawn(this.pawnPositions.getX(), this.pawnPositions.getY()));
   }
 
   @Test
-  public void testMovement() {
+  public void testWrongPawnPosition() {
+    Pair<Integer, Integer> wrongPosition = new Pair<Integer, Integer>((this.pawnPositions.getX() + 1) % fieldSize,
+    (this.pawnPositions.getY() + 1) % fieldSize);
+    assertFalse(this.logic.hasPawn(wrongPosition.getX(), wrongPosition.getY()));
+  }
+
+  @Test
+  public void testKnightMovement() {
     int newRowKnight = this.knightPosition.getX() + 2;
     int newColumnKnight = this.knightPosition.getY() + 1;
     Pair<Integer, Integer> newKnightPosition = new Pair<Integer, Integer>(newRowKnight, newColumnKnight);
@@ -48,11 +62,15 @@ public class LogicTest {
   @Test
   public void testMovements() {
     int movementsNumber = 20;
-    IntStream.range(0, movementsNumber).forEach(step -> this.testMovement());
+    IntStream.range(0, movementsNumber).forEach(step -> this.testKnightMovement());
   }
 
   private Pair<Integer, Integer> getKnightPosition() {
-    return this.getFulfilPosition(element -> this.logic.hasKnight(element.getX(), element.getY()));
+    return this.getFulfilPosition(knightPosition -> this.logic.hasKnight(knightPosition.getX(), knightPosition.getY()));
+  }
+
+  private Pair<Integer, Integer> getPawnPosition() {
+    return this.getFulfilPosition(pawnPosition -> this.logic.hasPawn(pawnPosition.getX(), pawnPosition.getY()));
   }
 
   private Pair<Integer, Integer> getFulfilPosition(Predicate<Pair<Integer, Integer>> predicate) {
