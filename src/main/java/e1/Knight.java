904d2ac1ba837075269bbdd09a8bc5c37e390d0f
Angelo Tinti <angelo.tinti@studio.unibo.it>
2024-03-05 17:52:41 +0100

    Change println in Knigh class

--- src/main/java/e1/Knight.java
+++ src/main/java/e1/Knight.java
@@ -12,7 +12,7 @@ public class Knight extends ChessPieceAbstract {
 
     @Override
     public boolean isMovementLogicValid(int row, int column) {
-        System.out.println("KNIGHT POSITION - " + this.getPosition());
+        System.out.println("OLD KNIGHT POSITION - " + this.getPosition());
         return Math.abs((this.getPosition().getX() - row) * (this.getPosition().getY() - column)) == 2;
     }
 
