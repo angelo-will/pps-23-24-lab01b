9669158e0a79804d76ec2b36a03c778f68b17368
Angelo Tinti <angelo.tinti@studio.unibo.it>
2024-03-05 17:59:42 +0100

    Add javadoc to Movement and ChessPiece Interface

--- src/main/java/e1/ChessPiece.java
+++ src/main/java/e1/ChessPiece.java
@@ -1,7 +1,21 @@
 package e1;
 
 public interface ChessPiece extends Movement {
+
+    /**
+     * Get the position of chess piece.
+     * 
+     * @return position of this chess piece
+     */
     Pair<Integer, Integer> getPosition();
 
-    void setPosition(int row, int column);
+    
+    /**
+     * Set the new position of chess piece.
+     * 
+     * @param row new row of piece
+     * @param column new column of piece
+     * @throws IllegalArgumentException if row or column are negative
+     */
+    void setPosition(int row, int column) throws IllegalArgumentException;
 }
--- src/main/java/e1/Movement.java
+++ src/main/java/e1/Movement.java
@@ -2,7 +2,21 @@ package e1;
 
 public interface Movement {
 
+    /**
+     * Perform movement if possible, throws exception otherwise.
+     * 
+     * @param row the new row
+     * @param column the new colum
+     * @throws IllegalArgumentException if movement logic is not respected
+     */
     void move(int row, int column) throws IllegalArgumentException;
 
+    /**
+     * Check if row and column passed fulfil movement logic.
+     * 
+     * @param row the new row
+     * @param column the new column
+     * @return true if row and column respect the movement logic
+     */
     boolean isMovementLogicValid(int row, int column);
 }
